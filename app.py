408c163322e1061c3f81b19be74e8b2c50e78bd5
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-04 09:37:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ if page == "OOS Projection WH":
 
     # Show table with only logic columns
 
-    selected_data["Verdict"] = selected_data.apply(lambda row: "Tidak Aman" if row["Landed DOI"] < 5 else "Aman", axis=1)
+        selected_data["Verdict"] = selected_data.apply(lambda row: "Tidak Aman" if row["Landed DOI"] < 5 else "Aman", axis=1)
     
     st.markdown("<b><span style='font-size:26px; color:#20639B;'>Comparison Table</span></b>", unsafe_allow_html=True)
     #st.write("### Comparison Table")
