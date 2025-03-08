8d2c7ba0ece3af3f56c4d392c1f509fa40129437
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-08 19:58:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(layout="wide")
 file_paths = {
     "Logic A": "logic a.csv",
     "Logic B": "logic b.csv",
-    "Logic C": "logic c_new.csv",
+    "Logic C": "logic c new.csv",
     "Logic D": "logic d.csv",
 }
 
