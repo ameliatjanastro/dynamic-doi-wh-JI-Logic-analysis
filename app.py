6a87bc101130beebee2293c727403411586ef5df
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-08 20:23:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -160,12 +160,12 @@ if page == "OOS Projection WH":
 
     # Show table with only logic columns
 
-    selected_data["Verdict"] = selected_data.apply(lambda row: "Tidak Aman" if row["Landed DOI"] < 5 else "Aman", axis=1)
-    
-    st.markdown("<b><span style='font-size:26px; color:#20639B;'>Comparison Table</span></b>", unsafe_allow_html=True)
-    #st.write("### Comparison Table")
-    table_columns = ["Logic", "coverage", "New RL Qty", "New RL Value", "New DOI Policy WH", "Landed DOI", "Verdict"] #"Landed DOI - JI", 
-    original_dtypes = selected_data.dtypes
+        selected_data["Verdict"] = selected_data.apply(lambda row: "Tidak Aman" if row["Landed DOI"] < 5 else "Aman", axis=1)
+        
+        st.markdown("<b><span style='font-size:26px; color:#20639B;'>Comparison Table</span></b>", unsafe_allow_html=True)
+        #st.write("### Comparison Table")
+        table_columns = ["Logic", "coverage", "New RL Qty", "New RL Value", "New DOI Policy WH", "Landed DOI", "Verdict"] #"Landed DOI - JI", 
+        original_dtypes = selected_data.dtypes
     
     def highlight_cells(val):
         if val == "Tidak Aman":
